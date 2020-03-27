e22bd7d715c39453ab248f536f803485ee5fdbbe
Tave <Tave@DESKTOP-MU8QA6A>
2020-03-27 14:08:00 -0600

    //Se creo la clase empleado

---
+++ TestGit2/src/org/model/empleado/Empleado.java
@@ -0,0 +1,29 @@
+package org.model.empleado;
+
+public class Empleado {
+	
+	private int id;
+	private String name;
+	private String salario;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSalario() {
+		return salario;
+	}
+	public void setSalario(String salario) {
+		this.salario = salario;
+	}
+
+}
